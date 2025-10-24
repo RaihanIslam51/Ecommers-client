11db5fe2969ae37cd414af362c5262ad06abe8e1
RaihanIslam51 <mdraihan51674@gmail.com>
2025-10-24 17:33:54 +0600

    Home page compleate

--- src/app/Home/Banner/Components/RightBanner.jsx/LeftBanner.jsx
+++ src/app/Home/Banner/Components/RightBanner.jsx/LeftBanner.jsx
@@ -16,7 +16,7 @@ const LeftBanner = () => {
 
       {/* Content */}
       <div className="relative z-10 flex flex-col justify-center h-full px-4 md:px-6">
-        <h3 className="text-lg md:text-2xl font-bold mb-2 md:mb-4">
+        {/* <h3 className="text-lg md:text-2xl font-bold mb-2 md:mb-4">
           Special Offers
         </h3>
         <p className="text-sm md:text-base mb-4">
@@ -24,7 +24,7 @@ const LeftBanner = () => {
         </p>
         <button className="bg-green-600 hover:bg-green-700 transition  px-3 py-2 md:px-5 md:py-3 rounded-lg font-semibold w-max">
           Explore
-        </button>
+        </button> */}
       </div>
     </div>
   );
--- src/app/Home/Banner/Components/RightBanner.jsx/RightBanner.jsx
+++ src/app/Home/Banner/Components/RightBanner.jsx/RightBanner.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const RightBanner = () => {
   return (
-    <div className="relative text-black w-full h-64 md:h-96 bg-white rounded-lg overflow-hidden shadow-lg">
+    <div className="relative w-full h-64 md:h-96 bg-white rounded-lg overflow-hidden shadow-lg">
       {/* Background image */}
       <Image
         src="https://i.ibb.co/b5NrY9ZS/image-Right.jpg"
@@ -19,7 +19,7 @@ const RightBanner = () => {
 
       {/* Content */}
       <div className="relative z-10 flex flex-col justify-center h-full px-6 md:px-12 ">
-        <h2 className="text-xl md:text-4xl font-bold mb-2 md:mb-4">
+        {/* <h2 className="text-xl md:text-4xl font-bold mb-2 md:mb-4">
           Exclusive Deals on Electronics
         </h2>
         <p className="text-sm md:text-lg mb-4 md:mb-6">
@@ -27,7 +27,7 @@ const RightBanner = () => {
         </p>
         <button className="bg-blue-600 hover:bg-blue-700 transition  px-4 py-2 md:px-6 md:py-3 rounded-lg font-semibold w-max">
           Shop Now
-        </button>
+        </button> */}
       </div>
     </div>
   );
